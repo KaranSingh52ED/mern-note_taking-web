219376f2de45db7e4624bd63df1925d662692d07
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-04-17 15:04:25 +0530

    update

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -122,7 +122,7 @@ const SearchBar = () => {
             <p>{descriptionPopup.description}</p>
             <button
               className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-              onClick={() => toggleDescriptionPopup("")} // Pass an empty string or any other parameter you want to pass
+              onClick={() => toggleDescriptionPopup("")} // Close Popup
             >
               Close
             </button>
