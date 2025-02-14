2f735d2702391bfbb2d7a1073c607dc117ed6dbd
KaranSingh52ED <karansingh999703@gmail.com>
2025-02-15 01:21:57 +0530

    update backend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const authRoutes = require("./Routes/auth");
 const noteRoutes = require("./Routes/notes");
@@ -10,28 +9,37 @@ const noteRoutes = require("./Routes/notes");
 const app = express();
 const PORT = 6969;
 
+// Load environment variables
 dotenv.config();
+
+// Middleware
 app.use(cors());
-app.use(bodyParser.json());
 app.use(express.json());
 
+// Database Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connection Successful");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+};
+connectDB();
 
-try {
-    mongoose.connect(process.env.MONGO_URL);
-    console.log("Connection Successfull");
-} catch (error) {
-    console.log(error);
-}
-
+// Routes
 app.get("/", (req, res) => {
-    res.send("Server Is Running");
+  res.send("Server Is Running");
 });
-
 
 app.use("/auth", authRoutes);
 app.use("/notes", noteRoutes);
 app.use("/files", express.static("files"));
 
+// Start Server
 app.listen(PORT, () => {
-    console.log(`Server Running on Port ${PORT}`);
-})
+  console.log(`Server Running on Port ${PORT}`);
+});
